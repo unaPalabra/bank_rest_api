0e22ed2a2e6090e703dd082def4e93d960eeb485
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2024-04-07 22:45:50 +0700

    first part

---
+++ src/main/java/org/example/repository/OperationRepository.java
@@ -0,0 +1,19 @@
+package org.example.repository;
+
+import org.example.entity.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Integer> {
+    List<Operation> findOperationsByClientId(Long id);
+
+    @Query("SELECT b FROM Operation b WHERE b.client = :id AND b.time_operation >= :beginDate AND b.time_operation <= :endDate")
+    List<Operation> findOperationsByClientIdAndDateRange(@Param("id") Long id, @Param("beginDate") Date beginDate, @Param("endDate") Date endDate);
+
+}
