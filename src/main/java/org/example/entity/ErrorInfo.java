3cc99ab4c6d7ef128ad53c9b973440b34f130948
unaPalabra <122109082+unaPalabra@users.noreply.github.com>
2024-04-07 22:45:30 +0700

    first part

---
+++ src/main/java/org/example/entity/ErrorInfo.java
@@ -0,0 +1,5 @@
+package org.example.entity;
+
+public record ErrorInfo(String message) {
+}
+
---
+++ src/main/java/org/example/entity/Operation.java
@@ -0,0 +1,35 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "OPERATION")
+public class Operation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id_operation;
+
+    @Column(name = "type_operation")
+    private int type_operation;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @Column(name = "time_operation")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private Date time_operation;
+
+    @ManyToOne
+    private Client client;
+
+}
